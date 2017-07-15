ddeb01def825ae02a2c3153d8fda922bdae5b5dc
aaratikatla <aaratikatla@vmware.com>
2017-07-14 23:02:28 -0700

    Updated app

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ def home():
     #page = request.args.get('page')
     data = []
     return render_template('index.html', data = data)
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+@app.route('/driver')
+def driver():
+        return render_template('driver.html')
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True, host='localhost', port=4000)
+
