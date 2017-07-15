0151910a1703b3fa649e910dc7d3157f0ffab74c
aaratikatla <aaratikatla@vmware.com>
2017-07-15 10:08:10 -0700

    login

--- app.py
+++ app.py
@@ -23,42 +23,31 @@ def login_required(f):
 def login_page():
     return render_template('login.html')
 
-@app.route('/login', methods=['GET'])
+@app.route('/', methods=['GET'])
 @login_required
 def login_get():
-    return redirect(url_for('index'))
+    if session['username'] == 'User1' or session['username'] == 'User2':
+        return render_template("user_main.html")
+    else
+        return render_template("driverIndex.html")
 
 
-@app.route('/')
-def home():
-    #page = request.args.get('page')
-    data = []
-    return render_template('index.html', data = data)
-@app.route('/user')
-def get_main_user_page():
-    #page = request.args.get('page')
-    data = []
-    return render_template('user_main.html', data = data)
 @app.route('/send_package')
 def get_send_package_page():
-    #page = request.args.get('page')
     data = []
     return render_template('send_package.html', data = data)
+
+
 @app.route('/get_package')
 def get_receive_package_page():
-    #page = request.args.get('page')
     data = []
     return render_template('get_package.html', data = data)
+
+
 @app.route('/index',methods=['GET'])
 @login_required
 def index():
     return render_template('index.html')
-
-
-@app.route('/user',methods=['GET'])
-@login_required
-def user():
-    return render_template('user.html')
 
 
 @app.route('/logout')
@@ -68,23 +57,36 @@ def logout():
     session.pop('logged_in', None)
     return login_page()
 
+
 @app.errorhandler(404)
 @login_required
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
+
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    if request.form['username'] == 'admin' and request.form['password'] == 'password':
+    if request.form['username'] == 'User1' and request.form['password'] == '12345':
         session['user'] = request.form['username']
         session['logged_in'] = True
+        return render_template("user_main.html")
+    elif request.form['username'] == 'User2' and request.form['password'] == '12345':
+        session['user'] = request.form['username']
+        session['logged_in'] = True
+        return render_template("user_main.html")
+    elif request.form['username'] == 'Driver' and request.form['password'] == '12345':
+        session['user'] = request.form['username']
+        session['logged_in'] = True
+        return render_template("driverIndex.html")
     else:
         flash('Error! The credentials are not valid, please verify!')
-    return redirect(url_for('index'))
+        return redirect(url_for('login'))
+
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
