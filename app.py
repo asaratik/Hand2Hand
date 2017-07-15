c78e5e2c78eb816b4cb625fff3dd5860a343d29d
rahilvora <rahil.vora740@gmail.com>
2017-07-15 05:39:58 -0700

    Adds user changes

--- app.py
+++ app.py
@@ -9,10 +9,20 @@ def home():
     data = []
     return render_template('index.html', data = data)
 @app.route('/user')
-def user():
+def get_main_user_page():
     #page = request.args.get('page')
     data = []
     return render_template('user_main.html', data = data)
+@app.route('/send_package')
+def get_send_package_page():
+    #page = request.args.get('page')
+    data = []
+    return render_template('send_package.html', data = data)
+@app.route('/get_package')
+def get_receive_package_page():
+    #page = request.args.get('page')
+    data = []
+    return render_template('get_package.html', data = data)
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True, host='localhost', port=4000)
--- static/js/user_main.js
+++ static/js/user_main.js
@@ -1,3 +1,36 @@
 /**
  * Created by rahilvora on 7/14/17.
  */
+console.log("File loaded");
+$("#pickup").on('click', function(){
+    debugger;
+    setTimeout(function () {
+        $("#myModal").hide();
+        $("#myModa2").show();
+
+    }, 5000);
+});
+var secretCode = ["554-2355-998", "554-2325-998", "744-7655-934", "554-2555-945", "594-7345-945", "794-7385-945"];
+function random() {
+    debugger;
+    setTimeout(function () {
+        var indexOfSecretCode = Math.floor(Math.random() * 6) + 1; // Between 1 and 6
+        $(".modal-body").text("Share this code with the receiver to assure his availability. " + secretCode[indexOfSecretCode-1]);
+        $("#myModalLabel").text("Done");
+        //$("#myModa2").show();
+
+    }, 5000);
+}
+
+function acceptPackage() {
+    debugger;
+    console.log($("#package_number").val());
+    if(secretCode.indexOf($("#package_number").val()) == -1){
+        $(".modal-body").text("Secret Code is Invalid");
+        $("#myModalLabel").text("Done");
+    }
+    else {
+        $(".modal-body").text("Secret code accepted. Package is schedule for the delivary");
+        $("#myModalLabel").text("Done");
+    }
+}
